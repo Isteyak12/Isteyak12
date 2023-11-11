c560cd455dffb2b4e1ef866383a061bf380c8f4f
Isteyak <112750375+Isteyak12@users.noreply.github.com>
2023-11-11 12:16:53 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-    st.title('Dockerized Streamlit App')
-    st.write("Hello, this is a Streamlit app running in Docker!")
+st.title('Dockerized Streamlit App')
+st.write("Hello, this is a Streamlit app running in Docker!")
 
 
