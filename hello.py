68cc62dc80e6c7e16ed3ca8d153fb911e1e3228e
Isteyak <112750375+Isteyak12@users.noreply.github.com>
2023-11-11 11:47:12 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title('Dockerized Streamlit App')
+    st.write("Hello, this is a Streamlit app running in Docker!")
+
+if __name__ == '__main__':
+    main()
+
